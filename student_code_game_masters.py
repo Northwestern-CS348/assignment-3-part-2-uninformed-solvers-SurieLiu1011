95256b123224b8d1fac4cd33aeb38325158344a8
XijingLiu <xijingliu2020@u.northwestern.edu>
2019-02-21 11:02:46 -0600

    Xijing Liu

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -34,8 +34,100 @@ class TowerOfHanoiGame(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### student code goes here
-        pass
 
+        CurrentState = []
+        # calculate the number of disks in KB:
+        #NumberOfDisk = 0
+        #for facts in self.kb.facts:
+        #    if facts.statement.terms[1] == "disk":
+        #        NumberOfDisk += 1
+        #if NumberOfDisk > 5:
+        #    print("Please use less than 5 disks.")
+        # find the matched facts:
+        '''
+        for facts in self.kb.facts:
+            if (facts.statement.predicate == "on") and (facts.statement.terms[1] == Constant("peg1")):
+                if facts.statement.terms[0] == Constant("disk1"):
+                    CurrentState[0].append(1)
+                elif facts.statement.terms[0] == Constant("disk2"):
+                    CurrentState[0].append(2)
+                elif facts.statement.terms[0] == Constant("disk3"):
+                    CurrentState[0].append(3)
+                elif facts.statement.terms[0] == Constant("disk4"):
+                    CurrentState[0].append(4)
+                else:
+                    CurrentState[0].append(5)
+
+
+            elif facts.statement.predicate == "on" and facts.statement.terms[1] == Constant("peg2"):
+                if facts.statement.terms[0] == Constant("disk1"):
+                    CurrentState[1].append(1)
+                elif facts.statement.terms[0] == Constant("disk2"):
+                    CurrentState[1].append(2)
+                elif facts.statement.terms[0] == Constant("disk3"):
+                    CurrentState[1].append(3)
+                elif facts.statement.terms[0] == Constant("disk4"):
+                    CurrentState[1].append(4)
+                else:
+                    CurrentState[1].append(5)
+
+            elif facts.statement.predicate == "on" and facts.statement.terms[1] == Constant("peg3"):
+                if facts.statement.terms[0] == Constant("disk1"):
+                    CurrentState[2].append(1)
+                elif facts.statement.terms[0] == Constant("disk2"):
+                    CurrentState[2].append(2)
+                elif facts.statement.terms[0] == Constant("disk3"):
+                    CurrentState[2].append(3)
+                elif facts.statement.terms[0] == Constant("disk4"):
+                    CurrentState[2].append(4)
+                else:
+                    CurrentState[2].append(5)
+
+        #print(CurrentState)
+        # ascending order:
+        for i in range(0, len(CurrentState)):
+            CurrentState[i].sort()
+
+        # transfer to ():
+        for i in range(0, len(CurrentState)):
+            CurrentState[i] = tuple(CurrentState[i])
+        GetState = tuple(CurrentState)
+
+        return GetState
+
+        #pass
+        '''
+        ask1 = parse_input("fact: (on ?x peg1")
+        ask2 = parse_input("fact: (on ?x peg2")
+        ask3 = parse_input("fact: (on ?x peg3")
+        answer1 = self.kb.kb_ask(ask1)
+        answer2 = self.kb.kb_ask(ask2)
+        answer3 = self.kb.kb_ask(ask3)
+        answers = [answer1, answer2, answer3]
+
+        for item in answers:
+            peg = []
+            if item:
+                for an in item:
+                    disk = int(str(an).split(":")[1][-1])
+                    peg.append(disk)
+                peg.sort()
+                CurrentState.append(peg)
+            else:
+                CurrentState.append([])
+
+        for i in range(0, len(CurrentState)):
+            CurrentState[i] = tuple(CurrentState[i])
+        GetState = tuple(CurrentState)
+
+        return GetState
+    def findTargetPeg(self, term):
+        if term == Constant("peg1"):
+            return 1
+        elif term == Constant("peg2"):
+            return 2
+        else:
+            return 3
     def makeMove(self, movable_statement):
         """
         Takes a MOVABLE statement and makes the corresponding move. This will
@@ -53,7 +145,176 @@ class TowerOfHanoiGame(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        #pass
+
+        #print("movable_statement:", movable_statement)
+        Current_state = self.getGameState()
+        tar_peg = self.findTargetPeg(movable_statement.terms[2])
+        length = len(Current_state[tar_peg-1])
+        if length == 0:
+            # target is empty:
+            #new_s1 = Statement(["top", movable_statement.terms[0], movable_statement.terms[2]])
+            #new_fac1 = Fact(new_s1, [])
+            new_s2 = Statement(["on", movable_statement.terms[0], movable_statement.terms[2]])
+            new_fac2 = Fact(new_s2, [])
+            #self.kb.kb_add(new_fac1)
+            self.kb.kb_add(new_fac2)
+            old_s1 = Statement(["top", movable_statement.terms[0], movable_statement.terms[1]])
+            old_s2 = Statement(["on", movable_statement.terms[0], movable_statement.terms[1]])
+            old_s3 = Statement(["empty", movable_statement.terms[2]])
+            self.kb.kb_retract(Fact(old_s1))
+            self.kb.kb_retract(Fact(old_s2))
+            self.kb.kb_retract(Fact(old_s3))
+        else:
+            # target is not empty:
+            #new_s1 = Statement(["top", movable_statement.terms[0], movable_statement.terms[2]])
+            #new_fac1 = Fact(new_s1, [])
+            new_s2 = Statement(["on", movable_statement.terms[0], movable_statement.terms[2]])
+            new_fac2 = Fact(new_s2, [])
+            #self.kb.kb_add(new_fac1)
+            self.kb.kb_add(new_fac2)
+            old_s1 = Statement(["top", movable_statement.terms[0], movable_statement.terms[1]])
+            old_s2 = Statement(["on", movable_statement.terms[0], movable_statement.terms[1]])
+            old_s3 = Statement(["top", "disk"+str(Current_state[tar_peg-1][0]), movable_statement.terms[2]])
+            self.kb.kb_retract(Fact(old_s1))
+            self.kb.kb_retract(Fact(old_s2))
+            self.kb.kb_retract(Fact(old_s3))
+        # add initial peg information:(empty & top)
+        Current_state = self.getGameState()
+        for i in range(0, len(Current_state)):
+
+            if len(Current_state[i]) == 0:
+                new_emp_s = Statement(["empty", "peg"+str(i+1)])
+                new_emp = Fact(new_emp_s, [])
+                self.kb.kb_add(new_emp)
+            else:
+                new_top_s = Statement(["top", "disk"+str(Current_state[i][0]), "peg"+str(i+1)])
+                new_top = Fact(new_top_s, [])
+                self.kb.kb_add(new_top)
+
+
+
+
+
+        '''
+        for facts in self.kb.facts:
+
+            if match(movable_statement, facts.statement):
+                # delete pre state:
+
+                for facts in self.kb.facts:
+                    if facts.statement.predicate == "on" and facts.statement.terms[0] == movable_statement.terms[0] and facts.statement.terms[1] == movable_statement.terms[1]:
+                        facts = self.kb._get_fact(facts)
+                        self.kb.kb_retract(facts)
+                    if facts.statement.predicate == "top" and facts.statement.terms[0] == movable_statement.terms[0] and facts.statement.terms[1] == movable_statement.terms[1]:
+                        facts = self.kb._get_fact(facts)
+                        self.kb.kb_retract(facts)
+
+                # move:
+                new_statement = Statement(["on", movable_statement.terms[0], movable_statement.terms[2]])
+                new_fact = Fact(new_statement, [])
+                if new_fact in self.kb.facts:
+                    print("Error, please make a movement")
+                    return
+                else:
+                    self.kb.kb_assert(new_fact)
+                # find top:
+                new_state = self.getGameState()
+                s = False
+
+                if movable_statement.terms[1] == Constant("peg1"):
+                    if new_state[0] != ():
+                        index = new_state[0][0]
+                        s = self.findtop(index, "peg1")
+
+                elif movable_statement.terms[1] == Constant("peg2"):
+                    if new_state[1] != ():
+                        index = new_state[1][0]
+                        s = self.findtop(index, "peg2")
+                elif movable_statement.terms[1] == Constant("peg3"):
+                    if new_state[2] != ():
+                        index = new_state[2][0]
+                        s = self.findtop(index, "peg3")
+                else:
+                    print("error: only have 3 pegs")
+                    return
+                if s:
+                    new_top1 = Fact(s, [])
+                    if new_top1 not in self.kb.facts:
+                        self.kb.kb_assert(new_top1)
+
+                ss = Statement(["top", movable_statement.terms[0], movable_statement.terms[2]])
+                new_top2 = Fact(ss, [])
+                if new_top2 not in self.kb.facts:
+                    self.kb.kb_assert(new_top2)
+
+                #find empty:
+                # remove pre
+                if movable_statement.terms[2] == Constant("peg1"):
+                    pre_empty = Statement(["empty", "peg1"])
+                elif movable_statement.terms[2] == Constant("peg2"):
+                    pre_empty = Statement(["empty", "peg2"])
+                else:
+                    pre_empty = Statement(["empty", "peg3"])
+
+                for facts in self.kb.facts:
+                    if facts.statement == pre_empty:
+                        facts = self.kb._get_fact(facts)
+                        self.kb.kb_retract(facts)
+                # add new
+                for i in range(0, len(new_state)):
+                    if len(new_state[i]) == 0:
+                        sss = Statement(["empty", self.findempty(i+1)])
+                        new_empty = Fact(sss, [])
+                        if new_empty not in self.kb.facts:
+                            self.kb.kb_assert(new_empty)
+
+
+
+        #print(self.getGameState())
+        '''
+
+
+
+    '''
+        
+    def findtop(self, index, peg):
+
+        if index == 1:
+            new_statement = Statement(["top", "disk1", peg])
+        elif index == 2:
+            new_statement = Statement(["top", "disk2", peg])
+        elif index == 3:
+            new_statement = Statement(["top", "disk3", peg])
+        elif index == 4:
+            new_statement = Statement(["top", "disk4", peg])
+        elif index == 5:
+            new_statement = Statement(["top", "disk5", peg])
+        else:
+            print("error: only have 5 disks")
+            return
+        return new_statement
+
+    def findempty(self, index):
+        if index == 1:
+            return "peg1"
+        elif index == 2:
+            return "peg2"
+        elif index == 3:
+            return "peg3"
+        else:
+            print("error: only have 3 pegs")
+            return
+
+        '''
+
+
+
+
+
+
+
+
 
     def reverseMove(self, movable_statement):
         """
@@ -86,6 +347,29 @@ class Puzzle8Game(GameMaster):
         """
         return parse_input('fact: (movable ?piece ?initX ?initY ?targetX ?targetY)')
 
+    def getTileNumber(self, tile):
+        if tile == Constant("tile1"):
+            return 1
+        elif tile == Constant("tile2"):
+            return 2
+        elif tile == Constant("tile3"):
+            return 3
+        elif tile == Constant("tile4"):
+            return 4
+        elif tile == Constant("tile5"):
+            return 5
+        elif tile == Constant("tile6"):
+            return 6
+        elif tile == Constant("tile7"):
+            return 7
+        elif tile == Constant("tile8"):
+            return 8
+        elif tile == Constant("empty"):
+            return -1
+        else:
+            print("Only have 9 tiles")
+            return
+
     def getGameState(self):
         """
         Returns a representation of the the game board in the current state.
@@ -100,7 +384,74 @@ class Puzzle8Game(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### Student code goes here
-        pass
+        #pass
+
+
+        CurrentState = []
+        row1 = []
+        row2 = []
+        row3 = []
+        for facts in self.kb.facts:
+            if facts.statement.predicate == "position" and facts.statement.terms[2] == Constant("pos1"):
+                row1.append(facts.statement.terms[1].term.element)
+            elif facts.statement.predicate == "position" and facts.statement.terms[2] == Constant("pos2"):
+                row2.append(facts.statement.terms[1].term.element)
+            elif facts.statement.predicate == "position" and facts.statement.terms[2] == Constant("pos3"):
+                row3.append(facts.statement.terms[1].term.element)
+        # ascending order:
+        row1.sort()
+        row2.sort()
+        row3.sort()
+        CurrentState.append(row1)
+        CurrentState.append(row2)
+        CurrentState.append(row3)
+        for facts in self.kb.facts:
+            if facts.statement.predicate == "position":
+                if facts.statement.terms[2] == Constant("pos1"):
+                    for i in range(0, len(row1)):
+
+                        if row1[i] == facts.statement.terms[1].term.element:
+                            tilenumber = self.getTileNumber(facts.statement.terms[0])
+                            CurrentState[0][i] = tilenumber
+                elif facts.statement.terms[2] == Constant("pos2"):
+                    for i in range(0, len(row2)):
+
+                        if row2[i] == facts.statement.terms[1].term.element:
+                            tilenumber = self.getTileNumber(facts.statement.terms[0])
+                            CurrentState[1][i] = tilenumber
+                elif facts.statement.terms[2] == Constant("pos3"):
+                    for i in range(0, len(row3)):
+
+                        if row3[i] == facts.statement.terms[1].term.element:
+                            tilenumber = self.getTileNumber(facts.statement.terms[0])
+                            CurrentState[2][i] = tilenumber
+                '''
+        
+                for tiles in row1:
+                    if facts.statement.terms[2] == Constant("pos1"):
+
+                        if tiles == facts.statement.terms[1].term.element:
+                            tilenumber = self.getTileNumber(facts.statement.terms[0])
+                            CurrentState[0][0] = tilenumber
+                for tiles in row2:
+                    if facts.statement.terms[2] == Constant("pos2"):
+
+                        if tiles == facts.statement.terms[1].term.element:
+                            tilenumber = self.getTileNumber(facts.statement.terms[0])
+                            CurrentState[1].append(tilenumber)
+                for tiles in row3:
+                    if facts.statement.terms[2] == Constant("pos3"):
+
+                        if tiles == facts.statement.terms[1].term.element:
+                            tilenumber = self.getTileNumber(facts.statement.terms[0])
+                            CurrentState[2].append(tilenumber)
+                '''
+        # transfer to ():
+        for i in range(0, len(CurrentState)):
+            CurrentState[i] = tuple(CurrentState[i])
+        GetState = tuple(CurrentState)
+
+        return GetState
 
     def makeMove(self, movable_statement):
         """
@@ -119,7 +470,62 @@ class Puzzle8Game(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        #pass
+        #print("movable_statement:", movable_statement)
+        Current_state = self.getGameState()
+
+
+
+        # target is empty:
+        new_s1 = Statement(["position", movable_statement.terms[0], movable_statement.terms[3], movable_statement.terms[4]])
+        new_fac1 = Fact(new_s1, [])
+        new_s2 = Statement(["position", "empty", movable_statement.terms[1], movable_statement.terms[2]])
+        new_fac2 = Fact(new_s2, [])
+        self.kb.kb_add(new_fac1)
+        self.kb.kb_add(new_fac2)
+        old_s1 = Statement(["position", movable_statement.terms[0], movable_statement.terms[1], movable_statement.terms[2]])
+        old_s2 = Statement(["position", "empty", movable_statement.terms[3], movable_statement.terms[4]])
+        #old_s3 = Statement(["empty", movable_statement.terms[2]])
+        self.kb.kb_retract(Fact(old_s1))
+        self.kb.kb_retract(Fact(old_s2))
+        #self.kb.kb_retract(Fact(old_s3))
+
+
+        '''
+        for fact_move in self.kb.facts:
+
+            if match(movable_statement, fact_move.statement):
+                # delete pre state:
+
+                for facts in self.kb.facts:
+                    if facts.statement.predicate == "position" and facts.statement.terms[0] == movable_statement.terms[0] and facts.statement.terms[1] == movable_statement.terms[1] and facts.statement.terms[2] == movable_statement.terms[2]:
+                        facts = self.kb._get_fact(facts)
+                        self.kb.kb_retract(facts)
+                for facts in self.kb.facts:
+                    if facts.statement.predicate == "position" and facts.statement.terms[0] == Constant("empty") and facts.statement.terms[1] == movable_statement.terms[3] and facts.statement.terms[2] == movable_statement.terms[4]:
+                        facts = self.kb._get_fact(facts)
+                        self.kb.kb_retract(facts)
+                print("state after delete:", self.kb)
+                # move:
+                new_statement = Statement(["position", movable_statement.terms[0], movable_statement.terms[3], movable_statement.terms[4]])
+                new_fact = Fact(new_statement, [])
+                if new_fact in self.kb.facts:
+                    print("Error, please make a movement")
+                    return
+                else:
+                    self.kb.kb_assert(new_fact)
+
+                new_emptys = Statement(["position", "empty", movable_statement.terms[1], movable_statement.terms[2]])
+                new_empty = Fact(new_emptys, [])
+                if new_empty in self.kb.facts:
+                    print("Error, please make a movement")
+                    return
+                else:
+                    self.kb.kb_assert(new_empty)
+                print("state after move:", self.kb)
+
+        return
+'''
 
     def reverseMove(self, movable_statement):
         """
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -1,9 +1,10 @@
-
+from queue import Queue
 from solver import *
 
 class SolverDFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
+
 
     def solveOneStep(self):
         """
@@ -18,13 +19,79 @@ class SolverDFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
+
         ### Student code goes here
-        return True
+        #current_gamemaster = self.gm
+        current_gamestate = self.currentState
+        current_kb = current_gamestate.state
+        #print("current_kb:", current_kb)
+        self.visited[current_gamestate] = True
+        if self.currentState.state == self.victoryCondition:
+            return True
+        # get children:
+        movables = self.gm.getMovables()
+        #print("currentState: " + str(self.currentState.state))
+        #print("movable:")
+        #if movables:
+        #    for move in movables:
+        #        print(str(move))
+        #print("indexOfchildren:")
+        #print(current_gamestate.nextChildToVisit)
+
+        #print("movables: ", movables)
+        # if no children or children all visited, go back:
+        if len(movables) == 0 or current_gamestate.nextChildToVisit >= len(movables):
+            print("no valid children")
+            current_gamestate = current_gamestate.parent
+            if current_gamestate.requiredMovable is not None:
+                print("no valid children and reverse")
+                self.gm.reverseMove(current_gamestate.requiredMovable)
+        # expand if have unvisited children:
+        else:
+            print("expand")
+            move = movables[current_gamestate.nextChildToVisit]
+            self.gm.makeMove(move)
+            current_gamestate.nextChildToVisit += 1
+
+            # if child is the same as parent or child is visited, go back:
+            while ((current_gamestate.parent is not None) and (current_gamestate.parent.state == self.gm.getGameState())) or self.visited.__contains__(GameState(self.gm.getGameState(), 0, None)):
+                self.gm.reverseMove(move)
+                print("this child is the same as parent or visited")
+                if current_gamestate.nextChildToVisit >= len(movables):
+                    #self.currentState = current_gamestate.parent
+                    # all children have been visited and go back to parent node:
+                    self.gm.reverseMove(current_gamestate.requiredMovable)
+                    return False
+                else:
+                    move = movables[current_gamestate.nextChildToVisit]
+                    self.gm.makeMove(move)
+                    current_gamestate.nextChildToVisit += 1
+
+            new_state = self.gm.getGameState()
+            #print("new state:", new_state)
+            #print("movable:", self.gm.getMovables())
+
+            new_gamestate = GameState(new_state, current_gamestate.depth + 1, move)
+            current_gamestate.children.append(new_gamestate)
+            new_gamestate.parent = current_gamestate
+            self.currentState = new_gamestate
+
+        return False
+
 
 
 class SolverBFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
+        self.queue = Queue()
+        self.queue.put([self.currentState, []])
+        self.solveOneStep()
+
+    def to_origin(self):
+        curr_state = self.currentState
+        while curr_state.parent:
+            self.gm.reverseMove(curr_state.requiredMovable)
+            curr_state = curr_state.parent
 
     def solveOneStep(self):
         """
@@ -39,5 +106,39 @@ class SolverBFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
+
         ### Student code goes here
-        return True
+        if self.queue:
+            node = self.queue.get()
+            curr_state = node[0]
+            require_move = node[1]
+            self.visited[curr_state] = True
+            self.to_origin()
+            #("curr_state:", curr_state.state)
+            # to record in visit_list:
+            for item in require_move:
+                self.gm.makeMove(item)
+            if curr_state.state == self.victoryCondition:
+                return True
+            movebales = self.gm.getMovables()
+            for move in movebales:
+                self.gm.makeMove(move)
+                if ((curr_state.parent is not None) and (curr_state.parent.state == self.gm.getGameState())) or (self.visited.__contains__(GameState(self.gm.getGameState(), 0, None))):
+                    self.gm.reverseMove(move)
+                    continue
+                new_state = GameState(self.gm.getGameState(), curr_state.depth+1, move)
+                new_require = [re for re in require_move]
+                #print("old_require:" , new_require)
+                new_require.append(move)
+                #print("new_require: ", new_require)
+                curr_state.children.append(new_state)
+                new_state.parent = curr_state
+                self.visited[new_state] = True
+                self.queue.put([new_state, new_require])
+                self.gm.reverseMove(move)
+            #print("curr state", self.currentState.state)
+            self.currentState = curr_state
+            #print("new curr state", self.currentState.state)
+
+        return False
+
